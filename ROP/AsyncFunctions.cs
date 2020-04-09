70ccd84383667b8c3d9787e94942e947903a6128
davide.lettieri <davide.lettieri@insoore.com>
2020-04-09 20:38:18 +0200

    add then overload

--- ROP/AsyncFunctions.cs
+++ ROP/AsyncFunctions.cs
@@ -43,7 +43,8 @@ namespace ROP
             return await Bind(f)(r);
         }
 
-        public static Func<T, Task<Result<T>>> ToSwitchTaskFunction<T>(Func<T, Task> a)
+
+        internal static Func<T, Task<Result<T>>> ToSwitchTaskFunction<T>(Func<T, Task> a)
         {
             return async t =>
             {
@@ -51,34 +52,41 @@ namespace ROP
                 return t;
             };
         }
-
+        internal static Func<T, Task<Result<S>>> ToSwitchTaskFunction<T, S>(Func<T, Task<S>> a)
+        {
+            return async t =>
+            {
+                var r = await a(t);
+                return r;
+            };
+        }
         public static Func<Result<T>, Task<Result<TSuccess>>> Bind<T, TSuccess>(Func<T, Task<Result<TSuccess>>> f)
         {
             return r => r.Match(t => f(t), e => Task.FromResult(new Result<TSuccess>(e)));
         }
-
         public static Func<Result<T>, Result<TSuccess>> Bind<T, TSuccess>(Func<T, Result<TSuccess>> f)
         {
             return r => r.Match(t => f(t), e => new Result<TSuccess>(e));
         }
-
+        public async static Task<Result<SValue>> Then<TValue, SValue>(this Task<Result<TValue>> tr, Func<TValue, Task<SValue>> a)
+        {
+            var r = await tr;
+            return await Bind(ToSwitchTaskFunction(a))(r);
+        }
         public async static Task<Result<TValue>> Then<TValue>(this Task<Result<TValue>> tr, Func<TValue, Task> a)
         {
             var r = await tr;
             return await Bind(ToSwitchTaskFunction(a))(r);
         }
-
         public async static Task<Result<S>> Then<S, TValue>(this Task<Result<TValue>> tr, Func<TValue, Task<Result<S>>> f)
         {
             var r = await tr;
             return await Bind(f)(r);
         }
-
         public async static Task<Result<S>> Then<S, TValue>(this Result<TValue> r, Func<TValue, Task<Result<S>>> f)
         {
             return await Bind(f)(r);
         }
-
         public async static Task<Result<S>> Then<S, TValue>(this Task<Result<TValue>> tr, Func<TValue, Result<S>> f)
         {
             var r = await tr;
