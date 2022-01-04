abcb60c786796aa269465a7197bf59302d1c06d1
davidelettieri <davide.lettieri@gmail.com>
2022-01-04 17:14:16 +0100

    update target framework & minor changes

--- ROP/Failure.cs
+++ ROP/Failure.cs
@@ -6,7 +6,14 @@ namespace ROP
 {
     public class Failure
     {
+        /// <summary>
+        /// ReasonId is meant to be a code to be used internally to debug and troubleshoot issues
+        /// </summary>
         public string ReasonId { get; }
+        /// <summary>
+        /// Message is a description of the error, it is meant to be shown to the end user. Possibly localized in the user
+        /// language
+        /// </summary>
         public string Message { get; }
 
         public Failure(string reasonId, string message)
